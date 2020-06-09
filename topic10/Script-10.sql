419a0914f2f58bda065cad6c8ecb0c340aab6e03
Ivan Aponasevich <vano3546@gmail.com>
2020-06-09 11:17:33 +0300

    fix

--- topic10/Script-10.sql
+++ topic10/Script-10.sql
@@ -63,25 +63,27 @@ CREATE INDEX profiles_country_city_user_id_idx FROM profiles;
 */
 
 
-
-SELECT DISTINCT
-  communities.name,
- COUNT(communities_users.community_id) OVER () / 10 AS 'average_users',
-  MAX(profiles.birthday) OVER w AS 'youngest',
-  MIN(profiles.birthday) OVER w AS 'oldest',
-  COUNT(communities_users.user_id) OVER w AS 'amount_users',
-  COUNT(profiles.user_id) OVER() AS 'users_in_system',
-  COUNT(communities_users.user_id) OVER  w / COUNT(profiles.user_id) OVER() AS '%%'
+SELECT DISTINCT 
+  communities.name AS group_name,
+  COUNT(communities_users.user_id) OVER() 
+    / (SELECT COUNT(*) FROM communities) AS avg_users_in_groups,
+  FIRST_VALUE(users.first_name) OVER birthday_desc AS youngest_first_name,
+  FIRST_VALUE(users.last_name) OVER birthday_desc AS youngest_last_name,
+  FIRST_VALUE(users.first_name) OVER birthday_asc AS oldest_first_name,
+  FIRST_VALUE(users.last_name) OVER birthday_asc AS oldest_last_name,
+  COUNT(communities_users.user_id) OVER(PARTITION BY communities.id) AS users_in_group,
+  (SELECT COUNT(*) FROM users) AS users_total,
+  COUNT(communities_users.user_id) OVER(PARTITION BY communities.id) 
+    / (SELECT COUNT(*) FROM users) *100 AS '%%'
     FROM communities
-      JOIN communities_users
-        ON communities.id = communities_users.community_id
-      RIGHT JOIN profiles
-        ON communities_users.user_id = profiles.user_id
-     WINDOW w AS (PARTITION BY communities.name ORDER BY communities.name);
-       
-SELECT * FROM profiles;   
-SELECT * FROM communities_users;
-SELECT * FROM communities;
+      LEFT JOIN communities_users 
+        ON communities_users.community_id = communities.id
+      LEFT JOIN users 
+        ON communities_users.user_id = users.id
+      LEFT JOIN profiles 
+        ON profiles.user_id = users.id
+      WINDOW birthday_desc AS (PARTITION BY communities.id ORDER BY profiles.birthday DESC),
+             birthday_asc AS (PARTITION BY communities.id ORDER BY profiles.birthday); 
 
 
 
